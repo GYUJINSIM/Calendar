bff100925583a3ed0cff27654b29ecde68288bed
규진 <@220.72.194.185>
2019-02-12 23:50:51 +0900

    multiple Calendar

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -3,21 +3,29 @@ package calendar;
 import java.util.Scanner;
 
 public class Calendar {
-	private static final int[] MAX_DAYS = {31,28,31,30,31,30,31,31,30,31,30,31};
-	
-	public int MaxDaysOfMonth(int month){
-		return MAX_DAYS[month-1];
+	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	public int MaxDaysOfMonth(int month) {
+		return MAX_DAYS[month - 1];
 	}
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		System.out.print("출력하고싶은 달을 입력하시오:");
-		int month = scan.nextInt();
-		
 		Calendar cal = new Calendar();
-		System.out.print(month+"월은"+cal.MaxDaysOfMonth(month)+"일입니다.");
-		
-		scan.close();
-	}
 
+		Scanner scan = new Scanner(System.in);
+
+		System.out.print("반복횟수를 입력하시오:");
+		int multi = scan.nextInt();
+
+		
+		for (int i = 0; i < multi; i++) {	
+			System.out.print("출력하고싶은 달을 입력하시오:");			
+			int month = scan.nextInt();				
+			System.out.println(month + "월은" + cal.MaxDaysOfMonth(month) + "일까지입니다.");
+		}
+		System.out.println("Fin");
+		scan.close();
+		
+	}
+		
 }
